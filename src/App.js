11bc6dacbf73aa88ffe0978f3b5c6623df545218
AnilEnsari <anilensari@gmail.com>
2023-08-06 20:44:18 +0300

    needsupdates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Karakter from "./components/Karakter";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [page, setPage] = useState(1);
+  const [heroesData, setHeroesData] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/?page=${page}`).then((response) => {
+      //console.log(response);
+      setHeroesData(response.data);
+    });
+  }, [page]);
 
   return (
     <div className="App">
-      <h1 className="Header">Karakterler</h1>
+      <h1 className="Header">Karakterler </h1>
+
+      {heroesData.map((char, index) => {
+        return <Karakter char={char} key={index} />;
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,34 @@
 // Karakter bileşeniniz buraya gelecek
+import React, { useState } from "react";
+
+const Karakter = ({ char }) => {
+  console.log("char", char);
+  const [showInfo, setShowInfo] = useState(false);
+  if (!char) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="karakterler">
+      <button
+        className="charbuton"
+        onClick={() => {
+          setShowInfo(!showInfo);
+        }}
+      >
+        {char.name}
+      </button>
+      {showInfo && (
+        <div className="charInfo">
+          <div className="karakterlist">Gender: {char.gender}</div>
+          <div className="karakterlist">Height: {char.height}</div>
+          <div className="karakterlist">Mass: {char.mass}</div>
+          <div className="karakterlist">Birth Year: {char.birth_year}</div>
+          <div className="karakterlist">Eye Color: {char.eye_color}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Karakter;
